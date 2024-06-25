6dabfb94f1604ce34928a22f0ffc9a68f6041f16
twlite <46562212+twlite@users.noreply.github.com>
2024-06-25 23:03:16 +0545

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 8000);
 }
